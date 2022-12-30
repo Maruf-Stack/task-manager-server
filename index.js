ebd91da7c3182e533c6b2a7e6154b99b8b17de4c
Maruf <01780527702maruf@gmail.com>
2022-12-30 22:16:49 +0600

    jkf

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ async function run() {
             const result = await completedTashCollection.find(query).toArray()
             res.send(result)
         })
+        app.delete('/completedtask/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await completedTashCollection.deleteOne(filter);
+            res.send(result)
+        })
 
     }
     finally {
